830bbd988395fb0514ca6f3288a26fb788b7ef91
Tanvir <Tanvirislamnyc@gmail.com>
2024-09-24 21:25:47 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Orange from './assets/components/Orange.tsx'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello World</h1>
+     <Orange/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Orange.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Orange = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Orange</h1>
+    </>
+  );
+}
+
+export default Orange;
