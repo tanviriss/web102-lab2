eb2331f1cbc2589e891695ff30c303ae30d6f668
Tanvir <Tanvirislamnyc@gmail.com>
2024-09-24 21:47:36 -0400

    styled orange

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
-
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
       <div className='header'>
         <h1>Orange Selector</h1>
         <h2>Count:</h2>
-        <img className="orange" src="https://www.pngall.com/wp-content/uploads/2016/05/Orange-PNG-Clipart.png" alt="orange"/>
+        <img className="h-[15em] p-[1.5em] mx-auto text-center" src="https://www.pngall.com/wp-content/uploads/2016/05/Orange-PNG-Clipart.png" alt="orange"/>
       </div>
     </div>
   )
