7478329750252f2f09e376182a978ed9197aeb73
Tanvir <Tanvirislamnyc@gmail.com>
2024-09-24 21:59:56 -0400

    implemented count when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  
+
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => {
+    setCount(count + multiplier)
+  }
   return (
     <div className="App">
       <div className='header'>
         <h1>Orange Selector</h1>
-        <h2>Count:</h2>
-        <img className="h-[15em] p-[1.5em] mx-auto text-center" src="https://www.pngall.com/wp-content/uploads/2016/05/Orange-PNG-Clipart.png" alt="orange"/>
+        <h2>Count: {count}</h2>
+        <img onClick={updateCount} className="h-[30em] p-[2em] mx-auto text-center" src="https://www.pngall.com/wp-content/uploads/2016/05/Orange-PNG-Clipart.png" alt="orange"/>
       </div>
     </div>
   )
